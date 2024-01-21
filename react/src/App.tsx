a3e5e262aff6f4b3c27e192ef054fd564e9e9c4c
Ilya Patrzykat <ilyapatrzykat@Ilyas-MacBook-Air.local>
2024-01-20 22:32:13 -0800

    add changing rooms

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,16 +1,14 @@
-import React, { useEffect, useRef, useState } from 'react';
-import {
-  TGetVenueMakerOptions
-} from '@mappedin/mappedin-js';
-import '@mappedin/mappedin-js/lib/mappedin.css';
-import axios from 'axios';
+import React, { useEffect, useRef, useState } from "react";
+import { TGetVenueMakerOptions } from "@mappedin/mappedin-js";
+import "@mappedin/mappedin-js/lib/mappedin.css";
+import axios from "axios";
 import useMapView from "./useMapView";
 import useVenue from "./useVenue";
 
 const options: TGetVenueMakerOptions = {
-  mapId: '659efcf1040fcba69696e7b6',
-  key: '65a0422df128bbf7c7072349',
-  secret: '5f72653eba818842c16c4fdb9c874ae02100ffced413f638b7bd9c65fd5b92a4',
+  mapId: "659efcf1040fcba69696e7b6",
+  key: "65a0422df128bbf7c7072349",
+  secret: "5f72653eba818842c16c4fdb9c874ae02100ffced413f638b7bd9c65fd5b92a4",
 };
 
 interface Room {
@@ -32,19 +30,37 @@ export default function App() {
   const mapView = useMapView(mapRef.current, venue, {
     multiBufferRendering: true,
     xRayPath: true,
-    loadOptions: {
-    }});
+    loadOptions: {},
+  });
+  const [currentEndLocation, setCurrentEndLocation] = React.useState(0);
+
+  const endLocations = [
+    "CITR Radio LL502",
+    "INS Market LL301",
+    "Flavour Lab LL104",
+  ];
+
+  const incrementEndLocation = () => {
+    setCurrentEndLocation((currentEndLocation + 1) % endLocations.length);
+  };
+
+  const onPress = () => {
+    incrementEndLocation();
+  };
 
   useEffect(() => {
     const init = async () => {
       if (!mapView || !venue) return;
 
-      mapView.setBackgroundColor('#050505');
+      mapView.setBackgroundColor("#050505");
       const rooms = await get_rooms();
-      rooms.forEach(room => process_colours(room));
+      rooms.forEach((room) => process_colours(room));
 
-      const thresholds = rooms.filter(room => {
-        const totalUsage = room.usage.reduce((sum, current) => sum + current.value, 0);
+      const thresholds = rooms.filter((room) => {
+        const totalUsage = room.usage.reduce(
+          (sum, current) => sum + current.value,
+          0
+        );
         return totalUsage > room.threshold;
       });
 
@@ -56,31 +72,37 @@ export default function App() {
     };
 
     init();
-  }, [mapView, venue]);
+  }, [mapView, venue, currentEndLocation]);
 
   async function get_rooms() {
-    const response = await axios.get('http://localhost:3000/api/v1/object');
+    const response = await axios.get("http://localhost:3000/api/v1/object");
     const rooms: Room[] = response.data;
     return rooms;
   }
-  
+
   function process_colours(room: Room) {
-    const polygons = venue.locations.find(location => location.name === room.room_id)?.polygons;
-    const totalUsage = room.usage.reduce((sum, current) => sum + current.value, 0) / room.threshold;
+    const polygons = venue.locations.find(
+      (location) => location.name === room.room_id
+    )?.polygons;
+    const totalUsage =
+      room.usage.reduce((sum, current) => sum + current.value, 0) /
+      room.threshold;
     if (polygons) {
       if (totalUsage >= 1) {
-        mapView.setPolygonColor(polygons[0], '#FF0000');
+        mapView.setPolygonColor(polygons[0], "#FF0000");
       } else if (totalUsage >= 0.66) {
-        mapView.setPolygonColor(polygons[0], '#FF6347');
+        mapView.setPolygonColor(polygons[0], "#FF6347");
       } else {
-        mapView.setPolygonColor(polygons[0], '#FFA07A');
+        mapView.setPolygonColor(polygons[0], "#FFA07A");
       }
     }
   }
 
   function wayfinding(threshold: Room) {
-    const start = venue.locations.find((l) => l.name === 'LL201');
-    const end = venue.locations.find((l) => l.name === 'CITR Radio LL502');
+    const start = venue.locations.find((l) => l.name === "LL201");
+    const end = venue.locations.find(
+      (l) => l.name === endLocations[currentEndLocation]
+    );
     if (start && end) {
       console.error(start, end);
       mapView.Journey.draw(start.directionsTo(end), {
@@ -96,5 +118,10 @@ export default function App() {
     }
   }
 
-  return <div id='app' ref={mapRef} />;
+  return (
+    <>
+      <button onClick={onPress}>Press me to change route</button>
+      <div id="app" ref={mapRef} />
+    </>
+  );
 }
